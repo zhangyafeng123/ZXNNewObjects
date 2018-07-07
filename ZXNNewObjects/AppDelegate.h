c771a91bbf1ec1d97086d4834df1a49a0e24b3a5
张伟 <1203008460@qq.com>
2018-07-07 22:02:46 +0800

    Initial Commit

---
+++ ZXNNewObjects/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZXNNewObjects
+//
+//  Created by 张小牛 on 2018/7/7.
+//  Copyright © 2018年 ZXN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZXNNewObjects/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZXNNewObjects
+//
+//  Created by 张小牛 on 2018/7/7.
+//  Copyright © 2018年 ZXN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
